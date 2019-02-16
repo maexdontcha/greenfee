3a8fea5e00e8daad02962dca0f8c450f56e056ef
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-02-16 12:04:05 +0100

    heatmap, webpack

--- gov/index.js
+++ gov/index.js
@@ -5,11 +5,11 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('assets'))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   //res.send('Greenfee says hi!')
   res.render('index')
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Listening on port 3000!')
 })
