916a8be4ee93a8c792b1d7d2210edda5f6d7b5ca
Max Hänsel <mh@maxhaensel.de>
2019-02-16 16:06:24 +0100

    createAccountBalance

---
+++ mam/createAccountBalance.js
@@ -0,0 +1,29 @@
+const config = require('./config.json')
+const Mam = require('./lib/mam.client.js')
+const { asciiToTrytes, trytesToAscii } = require('@iota/converter')
+
+const mode = 'public'
+const provider = 'https://nodes.devnet.iota.org'
+
+let mamState = Mam.init(provider, config.seed, 2)
+let balance = 0
+const logData = data => {
+  const message = JSON.parse(trytesToAscii(data))
+  if (message.currentRateIOTA && message.amountExhaustEmissions) {
+    balance += message.currentRateIOTA * message.amountExhaustEmissions
+  }
+  // console.log('Fetched and parsed', JSON.parse(trytesToAscii(data)), '\n')
+}
+
+const createAccountBalance = async myRoot => {
+  return new Promise(async (resolve, reject) => {
+    await Mam.fetch(myRoot, mode, null, logData)
+    resolve(balance)
+  })
+}
+
+module.exports = createAccountBalance
+
+createAccountBalance(config.myRoot).then(data => {
+  console.log(data)
+})
