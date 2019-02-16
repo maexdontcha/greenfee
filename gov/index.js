d2adc36fb2fbb14b6267e92ae55df2d2abc84d79
manzanillo <manza@outlook.de>
2019-02-16 10:40:37 +0100

    added view

--- gov/index.js
+++ gov/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Greenfee says hi!')
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+  //res.send('Greenfee says hi!')
+  res.render('index')
 })
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('Listening on port 3000!')
 })
