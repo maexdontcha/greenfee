3a92bef403e691689f17727dae4fac89c3f778e4
manzanillo <manza@outlook.de>
2019-02-16 17:41:03 +0100

    show current prices

--- gov/frontend/js/map.js
+++ gov/frontend/js/map.js
@@ -1,34 +1,59 @@
 import heatmap from 'leaflet.heat'
 import leaflet_routing_machine from 'leaflet-routing-machine'
 
-const ACCESS_TOKEN = 'pk.eyJ1Ijoic3RhZG9sZiIsImEiOiJjanM3YjZvaGMwdzY5NDlybjlvNjIxZnYyIn0.9Hnl5-BzX0z3esU_Vzc8lA';
+const ACCESS_TOKEN =
+  'pk.eyJ1Ijoic3RhZG9sZiIsImEiOiJjanM3YjZvaGMwdzY5NDlybjlvNjIxZnYyIn0.9Hnl5-BzX0z3esU_Vzc8lA'
+
+const positions = []
 
 function makeMap() {
-    const mymap = L.map('themap').setView([48.779089, 9.172057], 12);
+  const mymap = L.map('themap').setView([48.779089, 9.172057], 12)
 
-    L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${ACCESS_TOKEN}`, {
-        maxZoom: 18,
-        attribution: '',
-        id: 'mapbox.streets'
-    }).addTo(mymap);
+  L.tileLayer(
+    `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${ACCESS_TOKEN}`,
+    {
+      maxZoom: 18,
+      attribution: '',
+      id: 'mapbox.streets'
+    }
+  ).addTo(mymap)
 
-    fetch('/getHeat').then(res => {
-        res.json().then(json => {
-            const heat = L.heatLayer(json.heat, { radius: 25 }).addTo(mymap);
-        });
-    });
+  fetch('/getHeat').then(res => {
+    res.json().then(json => {
+      const heat = L.heatLayer(json.heat, { radius: 25 }).addTo(mymap)
+    })
+  })
 
-    const routing = L.Routing.control({
+  /*  const routing = L.Routing.control({
         waypoints: [
             L.latLng(48.8201197, 9.2593979),//BOSCH Autowerkstatt Fellbach
             L.latLng(48.793130, 9.061087)
         ]
     }).addTo(mymap);
 
-    console.log(routing.getPlan())
+    console.log(routing.getPlan())*/
 
+  mymap.on('click', function(e) {
+    var popLocation = e.latlng
+    fetch('/getPrice?lat=' + popLocation.lat + '&lon=' + popLocation.lng).then(
+      res => {
+        res.json().then(json => {
+          if (json.price) {
+            var popup = L.popup()
+              .setLatLng(popLocation)
+              .setContent(
+                '<p>The price is <br />' +
+                  json.price.toFixed(2) +
+                  ' ' +
+                  json.unit +
+                  '</p>'
+              )
+              .openOn(mymap)
+          }
+        })
+      }
+    )
+  })
 }
 
-export {
-    makeMap
-}
+export { makeMap }
--- gov/index.js
+++ gov/index.js
@@ -6,50 +6,25 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('assets'))
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render('index')
 })
 
+app.disable('etag')
+
 // http://localhost:3000/getPrice?lat=1&long=2
-app.get('/getPrice', function (req, res) {
-  const distance = req.query.radius || 10000
+app.get('/getPrice', function(req, res) {
+  const distance = req.query.radius || 1000
 
-  pollution(req.query.lat, req.query.lon, distance).then(result => {
-
-    res.json({
-      status: 'ok',
-      averageEmission: result.averageEmission,
-      price: result.price,
-      unit: 'cent'
+  pollution(req.query.lat, req.query.lon, distance)
+    .then(result => {
+      res.json({
+        status: 'ok',
+        averageEmission: result.averageEmission,
+        price: result.price,
+        unit: 'cent'
+      })
     })
-
-  }).catch(err => {
-    res.json({
-      status: 'error',
-      err
-    })
-  })
-})
-
-
-app.get('/getHeat', function (req, res) {
-  const distance = req.query.radius || 15000
-
-  pollution(req.query.lat, req.query.lon, distance).then(result => {
-    const entries = result.entries;
-    const heat = [];
-    for (var i in entries) {
-      let heatpoint = []
-      if (entries[i].fields) {
-        heatpoint.push(entries[i].fields.location[0])
-        heatpoint.push(entries[i].fields.location[1])
-        heatpoint.push(entries[i].fields.value / 3)
-      }
-
-      heat.push(heatpoint)
-    }
-    res.json({ heat: heat })
-  })
     .catch(err => {
       res.json({
         status: 'error',
@@ -58,6 +33,33 @@ app.get('/getHeat', function (req, res) {
     })
 })
 
-app.listen(3000, function () {
+app.get('/getHeat', function(req, res) {
+  const distance = req.query.radius || 15000
+
+  pollution(req.query.lat, req.query.lon, distance)
+    .then(result => {
+      const entries = result.entries
+      const heat = []
+      for (var i in entries) {
+        let heatpoint = []
+        if (entries[i].fields) {
+          heatpoint.push(entries[i].fields.location[0])
+          heatpoint.push(entries[i].fields.location[1])
+          heatpoint.push(entries[i].fields.value / 3)
+        }
+
+        heat.push(heatpoint)
+      }
+      res.json({ heat: heat })
+    })
+    .catch(err => {
+      res.json({
+        status: 'error',
+        err
+      })
+    })
+})
+
+app.listen(3000, function() {
   console.log('Listening on port 3000!')
 })
